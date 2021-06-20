db0c9f4361f9ad9c467246e53fff94ab17300660
Pham-hung <khaipikin0@gmail.com>
2021-06-20 19:00:29 +0700

    Signed-off-by: Pham-hung <khaipikin0@gmail.com>

--- web-restaurant/routes/web.php
+++ web-restaurant/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
+});
+Route::get('login', function () {
+    return view('layouts.login');
 });
